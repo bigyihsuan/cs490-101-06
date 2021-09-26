b556b28067c81dc3cfb5b16ec3b1aa65445d28d4
Nova <kt359@njit.edu>
2021-09-26 11:32:46 -0400

    2

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import LoginForm from './components/LoginForm';
 function App() {
   
     const adminUser = {   
-      name: "john",
+      name: "admin",
       password: "1234"
     }
-  
+    
+    const userOne = {   
+      name: "user",
+      password: "1234"
+    }
+
     const [user, setUser] = useState({name:""});
     const [error, setError] = useState("");
 
     const Login = details => {
       console.log(details);
       if (details.name == adminUser.name && details.password == adminUser.password){
-        console.log("Logged In");
+        console.log("Logged In Admin");
+        setUser({
+          name: details.name
+        });
+      }
+      else if(details.name == userOne.name && details.password == userOne.password){
+        console.log("Logged In UserOne");
         setUser({
           name: details.name
         });
       }
       else{
         console.log("Do not match");
-        setError("Do not match");
+        setError("Failed Login");
       }
     }
 
