9361880d8dee78815b2da06107d7615d1c2a1045
bigyihsuan <bigyihsuan@gmail.com>
2021-09-27 00:27:40 -0400

    fix dir stuff

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 
 // sql boilerplate
-include("../account.php");
+include("account.php");
 
 function safe_get($name)
 {
