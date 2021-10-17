76a9f5680475f8f56d233cbb576dc1ffd8652587
bigyihsuan <bigyihsuan@gmail.com>
2021-10-17 11:56:00 -0400

    move php code into the login page

--- util/generate-git-report.sh
+++ util/generate-git-report.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# usage: (assuming root project dir)
+# bash util/generate-git-report.sh
+# outputs your changes to a .txt file
+# submit that with your submission
+
 authors=("Nova" "bigyihsuan" "pbp38")
-for author in ${authors[@]}; do
-    echo $author
-    git show `git log --author=$author | grep ^commit | awk -F ' ' '{print $2}'` > $author.txt
+for author in "${authors[@]}"; do
+    echo "$author"
+    git show "$(git log --author=$author | grep ^commit | awk -F ' ' '{print $2}')" >"$author.txt"
 done
