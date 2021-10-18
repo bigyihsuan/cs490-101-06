af1fc1bec77c3fe9d862a4bc18b8fe9c1f5bba04
bigyihsuan <bigyihsuan@gmail.com>
2021-10-18 18:00:50 -0400

    some work on getting the form to post

--- backend/add_question.php
+++ backend/add_question.php
@@ -1,4 +1,13 @@
 <?php
-include("../account.php");
+// include("../account.php");
+// global $db;
 
-global $db;
+session_start();
+
+echo "<p>Hello world!</p>";
+
+foreach (array_keys($_POST) as $key) {
+    print "<p>$key $_POST[$key]</p>";
+}
+
+header("status:303 href='backend/add_question.php'");
