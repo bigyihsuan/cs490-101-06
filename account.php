120bea2d23c6d564c54e207f1c476af105b6d7a9
bigyihsuan <bigyihsuan@gmail.com>
2021-10-23 21:47:20 -0400

    create sample users

--- account.php
+++ account.php
@@ -10,9 +10,7 @@ $db = new mysqli($hostname, $username, $password, $project);
 if (mysqli_connect_errno()) {
     exit();
 }
-?>
 
-<!--
-cmd: mysql -h us-cdbr-east-04.cleardb.com -u b9960caeae98e0 -p5ee5fe3e
-database name: heroku_454624e55acb805
--->
+
+// cmd: mysql -h us-cdbr-east-04.cleardb.com -u b9960caeae98e0 -p5ee5fe3e
+// database name: heroku_454624e55acb805
