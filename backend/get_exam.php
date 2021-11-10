16fcbb3d5cedd22d909618252ca24e6a1f97329d
bigyihsuan <bigyihsuan@gmail.com>
2021-11-09 19:44:50 -0500

    begin get exam

---
+++ backend/get_exam.php
@@ -0,0 +1,23 @@
+<?php
+include("../account.php");
+global $db;
+
+$exam_id = $_POST["exam_id"];
+
+// get exam, and associated questions
+$get_exam =
+    "SELECT
+        `Exam`.`title` AS `exam_title`,
+        `Question`.`prompt` AS `question`,
+        `ExamQuestion`.`max_score` AS `max_score` 
+    FROM `Exam`
+    JOIN `ExamQuestion` ON `Exam`.`id`=`ExamQuestion`.`exam`
+    JOIN `Question` ON `ExamQuestion`.`question`=`Question`.`id`
+    WHERE `Exam`.`id`={$exam_id}";
+($result = $db->query($get_exam)) or die();
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+
+foreach ($rows as $row) {
+    // keys: exam_title, question, max_score
+    print_r($row);
+}
