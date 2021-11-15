0b2f5fef81c31aca95de133309d1d917f09cb05e
bigyihsuan <bigyihsuan@gmail.com>
2021-11-14 19:34:57 -0500

    acc

--- account.php
+++ account.php
@@ -9,19 +9,10 @@ $username = $url["user"];
 $password = substr($url["path"], 1);
 $project  = $url["pass"];
 
-// $hostname = "us-cdbr-east-04.cleardb.com";
-// $username = "b9960caeae98e0";
-// $password = "5ee5fe3e";
-// $project  = "heroku_454624e55acb805";
-
 $db = new mysqli($hostname, $username, $password, $project);
 if (mysqli_connect_errno()) {
     exit();
 }
-
-
-// cmd: mysql -h us-cdbr-east-04.cleardb.com -u b9960caeae98e0 -p5ee5fe3e
-// database name: heroku_454624e55acb805
 
 /* test users:
 tom (teacher): hello
