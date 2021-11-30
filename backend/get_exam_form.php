7993caf5e25889b3f66002a49c877041bfcf82ea
bigyihsuan <bigyihsuan@gmail.com>
2021-11-29 20:56:41 -0500

    see question max score when taking an exam

--- backend/get_exam_form.php
+++ backend/get_exam_form.php
@@ -17,7 +17,8 @@ $title = $result->fetch_assoc()['exam_title'];
 $get_exam_form = <<<SQL
 SELECT
     `ExamQuestion`.`id` AS `exam_question_id`,
-    `Question`.`prompt` AS `question_prompt`
+    `Question`.`prompt` AS `question_prompt`,
+    `ExamQuestion`.`max_score` AS `question_max_score`
 FROM `Question`
 JOIN `ExamQuestion` ON `ExamQuestion`.`question`=`Question`.`id`
 JOIN `Exam` ON `Exam`.`id`=`ExamQuestion`.`exam`
@@ -31,7 +32,7 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
 $questions = <<<HTML
 <h1>$title</h1>
 <p id="exam_id" style="display:none;">$exam_id</p>
-<table id="exam_questions" align="center" border="1px" style="width: 600px; line-height: 40px;">
+<table id="exam_questions" style="width: 600px; line-height: 40px; align: center; border: 1px;">
 HTML;
 
 foreach ($rows as $row) {
@@ -39,10 +40,12 @@ foreach ($rows as $row) {
     // error_log(print_r($row, true));
     $exam_question_id = $row['exam_question_id'];
     $question_prompt = $row['question_prompt'];
+    $question_max_score = $row['question_max_score'];
     $question_row = <<<HTML
     <tr>
         <td id="exam_question_{$exam_question_id}_id"style="display:none;">$exam_question_id</td>
         <td>$question_prompt</td>
+        <td>$question_max_score</td>
     </tr>
     <tr>
         <td><textarea id="exam_question_id{$exam_question_id}_answer" name="exam_question_{$exam_question_id}_answer" cols="40" rows="10"></textarea></td>
