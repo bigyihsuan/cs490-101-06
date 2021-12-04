a28d8dedd958613e13163b5dc8563b75381c993a
bigyihsuan <bigyihsuan@gmail.com>
2021-12-04 18:43:21 -0500

    add test cases to question bank view

--- webpages/question_bank.php
+++ webpages/question_bank.php
@@ -4,7 +4,8 @@ include("../account.php");
 // include("../data_models.php");
 global $db;
 
-$query = "SELECT
+$query = <<<SQL
+SELECT
 `Question`.`id` AS id,
 `Question`.`prompt` AS prompt,
 `DifficultyTypes`.`difficulty` AS difficulty,
@@ -16,7 +17,8 @@ $query = "SELECT
 FROM `Question`
 JOIN `DifficultyTypes` ON `Question`.`difficulty`=`DifficultyTypes`.`id`
 JOIN `CategoryTypes` ON `Question`.`category`=`CategoryTypes`.`id`
-JOIN `ConsTypes` ON `Question`.`cons`=`ConsTypes`.`id`";
+JOIN `ConsTypes` ON `Question`.`cons`=`ConsTypes`.`id`
+SQL;
 
 if ($_POST['category'] != "") {
     $category = $_POST['category'];
@@ -56,7 +58,7 @@ $query .= "\nORDER BY `Question`.`id`;";
 
 ($result = $db->query($query)) or die();
 $html = <<<HTML
-<table id="question_bank" class="sortable" align="center" border="1px" style="width: 600px; line-height: 40px;">
+<table id="question_bank" align="center" border="1px" style="width: 600px; line-height: 40px;">
     <tr>
         <th>Question ID</th>
         <th>Prompt</th>
@@ -71,24 +73,49 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
 $making_exam = isset($_POST['making_exam']) ? $_POST['making_exam'] : false;
 $display_none = $making_exam ? "" : 'style="display:none;"';
 foreach ($rows as $row) {
+    $question_id = $row['id'];
     $html .= <<<HTML
-        <tr id="question_{$row['id']}" class="item">
-            <td id="question_{$row['id']}_id">{$row['id']}</td>
-            <td id="question_{$row['id']}_prompt">{$row['prompt']}</td>
-            <td id="question_{$row['id']}_difficulty" sorttable_customkey="{$row['difficulty_id']}">{$row['difficulty']}</td>
-            <td id="question_{$row['id']}_category" sorttable_customkey="{$row['category_id']}">{$row['category']}</td>
-            <td id="question_{$row['id']}_constraint" sorttable_customkey="{$row['cons_id']}">{$row['cons']}</td>
-            <td id="question_{$row['id']}_max_score" style='display:none;'>
+        <tr id="question_{$question_id}" class="item">
+            <td id="question_{$question_id}_id">{$question_id}</td>
+            <td id="question_{$question_id}_prompt">{$row['prompt']}</td>
+            <td id="question_{$question_id}_difficulty" sorttable_customkey="{$row['difficulty_id']}">{$row['difficulty']}</td>
+            <td id="question_{$question_id}_category" sorttable_customkey="{$row['category_id']}">{$row['category']}</td>
+            <td id="question_{$question_id}_constraint" sorttable_customkey="{$row['cons_id']}">{$row['cons']}</td>
+            <td id="question_{$question_id}_max_score" style='display:none;'>
                 <input type="number" placeholder="Question Score"/>
             </td>
-            <td id="question_{$row['id']}_add_to_exam" {$display_none}>
-                <button type="button" onclick=addToExam('question_{$row['id']}')>Add to Exam</button>
+            <td id="question_{$question_id}_add_to_exam" {$display_none}>
+                <button type="button" onclick=addToExam('question_{$question_id}')>Add to Exam</button>
             </td>
-            <td id="question_{$row['id']}_remove_from_exam" style="display:none;">
-                <button type="button" onclick=removeFromExam('question_{$row['id']}')>Remove Question</button>
+            <td id="question_{$question_id}_remove_from_exam" style="display:none;">
+                <button type="button" onclick=removeFromExam('question_{$question_id}')>Remove Question</button>
             </td>
         </tr>
         HTML;
+    $get_test_cases_of_question = <<<SQL
+        SELECT TestCase.`id` AS test_case_id, `input`, `output`
+        FROM TestCase
+        JOIN QuestionTestCase ON QuestionTestCase.test_case=TestCase.id
+        JOIN Question ON QuestionTestCase.question=Question.id
+        WHERE QuestionTestCase.question=$question_id;
+    SQL;
+
+    error_log($get_test_cases_of_question);
+
+    ($result = $db->query($get_test_cases_of_question)) or die();
+    $test_cases = $result->fetch_all(MYSQLI_ASSOC);
+    foreach ($test_cases as $test_case) {
+        $input = $test_case['input'];
+        $output = $test_case['output'];
+
+        $html .= <<<HTML
+        <tr>
+            <td>$input</td>
+            <td>=></td>
+            <td>$output</td>
+        </tr>
+        HTML;
+    }
 }
 $html .= '</table>';
 
